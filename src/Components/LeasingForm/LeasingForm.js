0ccb08257837352d5d23a415e460b548c8568721
Velliton <evg92faut@gmail.com>
2022-10-09 02:25:07 +0700

    Some manipulation with style

--- src/Components/LeasingForm/LeasingForm.js
+++ src/Components/LeasingForm/LeasingForm.js
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Swal from "sweetalert2";
-
+import "./LeasingForm.scss";
 import {ReactComponent as LoaderIcon} from "../../assets/loader.svg"
 
 import {InputWithRange} from "../InputWithRange"
